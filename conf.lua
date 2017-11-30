35c6343005c55b0e19db1dfae790f85e97d93de0
Thiago Mendes <thiago@posteo.de>
2017-11-30 11:20:45 -0200

    assets/config
    Add sounds and a background
    Add a config.lua file

---
+++ conf.lua
@@ -0,0 +1,12 @@
+function love.conf(t)
+    t.identity      = "GtGLG"
+    t.version       = "0.10.2"
+
+    t.window.title  = "Uhu! First one!"
+    t.window.width  = 1024
+    t.window.height = 768
+
+    t.window.highdpi = true
+    t.window.fsaa   = 4
+    t.window.vsync  = true
+end
